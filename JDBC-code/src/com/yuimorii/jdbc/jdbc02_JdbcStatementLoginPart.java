13f0c5518bb3ca9aa522d0ef58483907b1d84171
YUIMORII <yuimoriistudio@gmail.com>
2022-12-29 11:53:20 -0600

    Update

--- JDBC-code/src/com/yuimorii/jdbc/jdbc02_JdbcStatementLoginPart.java
+++ JDBC-code/src/com/yuimorii/jdbc/jdbc02_JdbcStatementLoginPart.java
@@ -1,10 +1,9 @@
 package com.yuimorii.jdbc;
 
 import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.protocol.Resultset;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 /**
@@ -19,26 +18,48 @@ import java.util.Scanner;
 // 1. clarify the use of jdbc flow and detailed analysis of the internal design api method
 // 2. find the problem, lead to the preparedStatement method
 
-public class jdbc02_JdbcStatementLoginPart {
-    public static void main(String[] args) throws SQLException {
 
+//STEP:
+//输入账号和没密码
+//进行数据库信息查询(t_user)
+//反馈登录成功还是登录失败
+public class jdbc02_JdbcStatementLoginPart {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//      0. 获取用户输入
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your account: ");
         String account = scanner.nextLine();
+        System.out.println("Please enter your password");
         String password = scanner.nextLine();
         scanner.close();
 
 //        1.  Load and register the driver class
-        DriverManager.registerDriver(new Driver());
-
-
+        //通过反射触发(在反射里: 有一个加载类, 获取类的模版信息注册一次驱动)
+       Class.forName("com.mysql.cj.jdbc.Driver");
 
 //        2.  Create connection with Database
-       Connection connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/yuimoriiJDBC","root", "yuimorii");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/yuimoriiJDBC", "root", "yuimorii");
 //        3.  Create platform to execute SQL queries
+        Statement statement = connection.createStatement();
+
 
 //        4.  Execute SQL queries
+        String sql = "select * from t_user where account = '"+account+"' and password = '"+password+"' ;";
+        ResultSet resultSet = statement.executeQuery(sql);
 //        5.  Close resources
+        //进行结果集对象解析
+        if (resultSet.next()){
+            //只要向下移动，就是有数据 就是登录成功！
+            System.out.println("登录成功！");
+        }else{
+            System.out.println("登录失败！");
+        }
 
+        //关闭资源
+        resultSet.close();
+        statement.close();
+        connection.close();
 
     }
 }
